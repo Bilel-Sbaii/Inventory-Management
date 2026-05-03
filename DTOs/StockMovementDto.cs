8ec7814d02939221a8976bb9d0182d163ae1665a
BilelSbai <bilelsbai18@gmail.com>
2026-05-03 18:14:25 +0100

    add-stock-movement-workflow

---
+++ DTOs/StockMovementDto.cs
@@ -0,0 +1,46 @@
+﻿using Inventory_Management.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management.DTOs
+{
+  public class AddStockMovementTransactionRequest
+  {
+    [Required]
+    public MovementType MovementType { get; set; }
+
+    public DateTime? MovementDate { get; set; }
+
+    public string? Notes { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int SupplierId { get; set; }
+
+    [MinLength(1)]
+    public List<AddStockMovementDetailRequest> Details { get; set; } = new();
+  }
+
+  public class AddStockMovementDetailRequest
+  {
+    [Range(1, int.MaxValue)]
+    public int ProductId { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; set; }
+
+    [Range(0, double.MaxValue)]
+    public decimal UnitPrice { get; set; }
+  }
+
+  public class AddStockMovementTransactionResponse
+  {
+    public int StockMovementId { get; set; }
+    public MovementType Type { get; set; }
+  }
+
+  public enum AddStockMovementResult
+  {
+    InvalidProduct = 0,
+    InvalidSupplier = 1,
+    Success = 2
+  }
+}
---
+++ Services/Interfaces/IStockMovementService.cs
@@ -0,0 +1,9 @@
+﻿using Inventory_Management.DTOs;
+
+namespace Inventory_Management.Services.Interfaces
+{
+  public interface IStockMovementService
+  {
+    Task<AddStockMovementResult> AddStockMovement(AddStockMovementTransactionRequest request);
+  }
+}
